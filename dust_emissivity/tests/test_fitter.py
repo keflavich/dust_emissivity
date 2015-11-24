e6907d30db4febeb8f23028cbd80207f05fdae91
Brigitta Sipocz <bsipocz@gmail.com>
2015-11-24 17:16:03 +0000

    Making test taking into account avaibility of optional dependencies

--- dust_emissivity/tests/test_fitter.py
+++ dust_emissivity/tests/test_fitter.py
@@ -4,11 +4,32 @@ from astropy.tests.helper import pytest
 from ..blackbody import modified_blackbody
 from ..fit_sed import fit_modified_bb
 
-@pytest.mark.parametrize(('fitter','precision'),
-                         zip(('lmfit','mpfit','montecarlo'),
-                             (8,8,2)))
+# Check which fitters are available
+
+fitters = []
+try:
+    import lmfit
+    fitters.append(('lmfit', 8))
+except ImportError:
+    pass
+try:
+    import agpy
+    fitters.append(('mpfit', 8))
+except ImportError:
+    pass
+try:
+    import pymc
+    fitters.append(('montecarlo', 2))
+except ImportError:
+    pass
+
+SKIP_TESTS = len(fitters) == 0
+
+
+@pytest.mark.skipif('SKIP_TESTS')
+@pytest.mark.parametrize(('fitter', 'precision'), fitters)
 def test_fit_modified_bb(fitter, precision):
-    wavelengths = np.array([20,70,160,250,350,500,850,1100]) * u.um
+    wavelengths = np.array([20, 70, 160, 250, 350, 500, 850, 1100]) * u.um
     frequencies = wavelengths.to(u.Hz, u.spectral())
     temperature = 15 * u.K
     column = 1e22 * u.cm**-2
@@ -17,14 +38,17 @@ def test_fit_modified_bb(fitter, precision):
     err = 0.1 * flux
     np.random.seed(0)
     noise = np.random.randn(frequencies.size) * err
-    tguess, bguess, nguess = 20.*u.K,2.,10**21.5*u.cm**-2
+    tguess, bguess, nguess = 20.*u.K, 2., 10**21.5*u.cm**-2
 
     pars = fit_modified_bb(frequencies, flux+noise, err,
                            guesses=(tguess, bguess, nguess),
                            fitter=fitter)
 
-    answer = np.array([1.50147255e+01,   1.77886818e+00,   1.05578825e+22])
-    
-    np.testing.assert_approx_equal(answer[0], pars[0].value, significant=precision)
-    np.testing.assert_approx_equal(answer[1], pars[1], significant=precision)
-    np.testing.assert_approx_equal(answer[2], pars[2].value, significant=precision)
+    answer = np.array([1.50147255e+01, 1.77886818e+00, 1.05578825e+22])
+
+    np.testing.assert_approx_equal(answer[0], pars[0].value,
+                                   significant=precision)
+    np.testing.assert_approx_equal(answer[1], pars[1],
+                                   significant=precision)
+    np.testing.assert_approx_equal(answer[2], pars[2].value,
+                                   significant=precision)
