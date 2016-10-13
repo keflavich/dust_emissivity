a4b402d00cee87b1e3fe48aa480bf120807e25c6
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-10-13 11:39:30 -0600

    add mean opacities & a table integral

--- dust_emissivity/dust.py
+++ dust_emissivity/dust.py
@@ -4,9 +4,14 @@ Dust emissivity
 ===============
 """
 from . import blackbody
+import numpy as np
+import os
+import requests
+from astropy.table import Table
 from astropy import constants
 from astropy import units as u
 from numpy import exp,log
+from scipy.integrate import quad
 
 def kappa(nu, nu0=271.1*u.GHz, kappa0=0.0114*u.cm**2*u.g**-1, beta=1.75):
     """
@@ -29,6 +34,86 @@ def kappa(nu, nu0=271.1*u.GHz, kappa0=0.0114*u.cm**2*u.g**-1, beta=1.75):
         The power-law index governing kappa as a function of nu
     """
     return (kappa0*(nu.to(u.GHz,u.spectral())/nu0.to(u.GHz,u.spectral()))**(beta)).to(u.cm**2/u.g)
+
+def kappa_table(nu, url='http://www2.mpia-hd.mpg.de/homes/henning/Dust_opacities/Opacities/Ralf/Pol/0__comp.Gofn'):
+    """
+    Use an opacity table to compute kappa
+
+    (does not work well with integrals-  results in an error)
+    """
+    if os.path.exists(os.path.basename(url)):
+        tbl = Table.read(os.path.basename(url), format='ascii.basic')
+    else:
+        response = requests.get(url)
+        with open(os.path.basename(url),'w') as fh:
+            fh.write(response.text)
+        tbl = Table.read(response.text, format='ascii.basic')
+
+    wavelengths = tbl["'lambda[um]"]*u.um
+    frequencies = wavelengths.to(u.Hz, u.spectral())
+    argsort = np.argsort(frequencies)
+
+    absorption = tbl["<Kabs>"]*u.cm**2/u.g
+
+    return np.interp(nu.to(u.Hz).value, frequencies[argsort].value,
+                     absorption[argsort].value)*u.cm**2/u.g
+
+def planck_average_kappa_table(temperature, weighting=blackbody.blackbody,
+                               normalization='blackbody',
+                               url='http://www2.mpia-hd.mpg.de/homes/henning/Dust_opacities/Opacities/Ralf/Pol/0__comp.Gofn',):
+
+    if os.path.exists(os.path.basename(url)):
+        tbl = Table.read(os.path.basename(url), format='ascii.basic')
+    else:
+        response = requests.get(url)
+        with open(os.path.basename(url),'w') as fh:
+            fh.write(response.text)
+        tbl = Table.read(response.text, format='ascii.basic')
+
+    wavelengths = tbl["'lambda[um]"]*u.um
+    frequencies = wavelengths.to(u.Hz, u.spectral())
+    argsort = np.argsort(frequencies)
+
+    absorption = tbl["<Kabs>"]*u.cm**2/u.g
+
+    dnu = np.diff(frequencies[argsort])
+
+    bb = weighting(frequencies[argsort][:-1], temperature)
+
+    integral = (bb * absorption[argsort][:-1] * dnu).sum()
+
+    if normalization == 'blackbody':
+        norm = constants.sigma_sb.cgs * temperature**4
+    else:
+        raise ValueError("Non-blackbody normalization not yet implemented")
+
+    return (integral / norm).to(u.cm**2/u.g)
+
+
+
+def planck_average_kappa(temperature, kappa=kappa, **kwargs):
+    """
+    Compute the Blackbody-weighted opacity assuming a power law dust
+    absorptivity
+    
+    THIS IS NOT PRESENTLY SANE: the opacity doesn't continue as a power law past ~100um
+    """
+    def kappabb(nu):
+        return (blackbody.blackbody(nu*u.Hz, temperature) * kappa(nu*u.Hz, **kwargs)).value
+
+    result = quad(kappabb, 1e9, 1e15, full_output=True)
+
+    if len(result) == 3:
+        integral,err,infodict = result
+    else:
+        raise ValueError("Integral did not converge or had some other problem."
+                         + str(result))
+
+    integral_unit = (blackbody.blackbody(1e5*u.Hz, temperature)*u.Hz * kappa(1e5*u.Hz)).decompose().unit
+
+    return (integral*integral_unit / (constants.sigma_sb.cgs * temperature**4)).to(u.cm**2/u.g)
+
+
 
 def snu(nu, column, kappa, temperature):
     """
